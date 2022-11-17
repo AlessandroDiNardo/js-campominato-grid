2ea2f637381a412e9ba024f6272d7bfc1f8e86fd
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-11-17 11:21:25 +0100

    esercizio completo di bonus

--- js/function.js
+++ js/function.js
@@ -8,3 +8,5 @@ function box() {
     // return
     return elem;
 }
+
+
--- js/main.js
+++ js/main.js
@@ -5,17 +5,75 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro e
 
 const myContainer = document.querySelector(".container");
 
-for (let i = 1; i <= 100; i++) {
-    
-    let newElem = box();
-    newElem.addEventListener("click",
-        function(){
-            newElem.classList.toggle("clicked");
+// apparizione livello
+let btnPlay = document.getElementById("play");
+btnPlay.addEventListener('click',
+    function(event) {
+        event.preventDefault()
+
+        let level = document.getElementById("difficult").value;
+        // easy level
+        if(level === "easy") {
+
+            myContainer.innerHTML = '';
+
+            for (let i = 1; i <= 100; i++) {
+
+                let newElem = box();
+                newElem.addEventListener("click",
+                        function(){
+                            newElem.classList.toggle("clicked");
+                            console.log("Levello easy, elemento selzionato: ", i);
+                        }
+                    );
+                
+                myContainer.appendChild(newElem);
+                newElem.append(i);
+                newElem.classList.add("easy");
+            }
         }
-    );
-    
-    myContainer.appendChild(newElem);
 
-    newElem.append(i);
-};
+        // medium level
+        if(level === "medium") {
 
+            myContainer.innerHTML = '';
+            
+            for (let i = 1; i <= 81; i++) {
+        
+                let newElem = box();
+                newElem.addEventListener("click",
+                        function(){
+                            newElem.classList.toggle("clicked");
+                            console.log("Levello medium, elemento selzionato: ", i);
+                        }
+                    );
+                
+                myContainer.appendChild(newElem);
+                newElem.append(i);
+                newElem.classList.add("medium");
+            }
+        }
+
+        // hard level
+        // medium level
+        if(level === "hard") {
+
+            myContainer.innerHTML = '';
+
+            for (let i = 1; i <= 49; i++) {
+        
+                let newElem = box();
+                newElem.addEventListener("click",
+                        function(){
+                            newElem.classList.toggle("clicked");
+                            console.log("Levello hard, elemento selzionato: ", i);
+                        }
+                    );
+                
+                myContainer.appendChild(newElem);
+                newElem.append(i);
+                newElem.classList.add("hard")
+            }
+        }
+    }
+);
